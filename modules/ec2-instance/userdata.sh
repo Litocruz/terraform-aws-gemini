fb47be9c67947f24b56ff2f218886db026240dd6
Litocruz <julian.lamadrid@gmail.com>
2025-08-14 17:34:14 +0200

    Se agregaron worflow manuales y se modifico script de instalacion por yum

--- modules/ec2-instance/userdata.sh
+++ modules/ec2-instance/userdata.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 # Actualizar la lista de paquetes
-sudo apt-get update -y
+sudo dnf update -y
 
 # Instalar Nginx
-sudo apt-get install nginx -y
+sudo dnf install nginx -y
 
 # Iniciar el servicio de Nginx
 sudo systemctl start nginx
