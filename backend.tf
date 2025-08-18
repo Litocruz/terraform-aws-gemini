9228584d4a70c6da26d984226224262a833119bc
Litocruz <julian.lamadrid@gmail.com>
2025-08-18 15:51:50 +0200

    Se saco el backend a una carpeta state sin versionar para la generacion del bucket s3

--- backend.tf
+++
@@ -1,9 +0,0 @@
-# backend.tf
-
-resource "aws_s3_bucket" "terraform_state_bucket" {
-  bucket = "mi-bucket-de-estado-terraform-devops" # ¡IMPORTANTE! El nombre debe ser único globalmente
-
-  tags = {
-    Name = "Terraform State Bucket"
-  }
-}
